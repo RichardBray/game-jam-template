7f8753eda53300d1699ec0b1b246c035bca7c643
Richard Bray <richardbray89@gmail.com>
2020-09-21 10:02:24 +0100

    Formatting

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,18 +1,15 @@
 package;
 
-import flixel.text.FlxText;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
-class PlayState extends FlxState
-{
-	override public function create()
-	{
+class PlayState extends FlxState {
+	override public function create() {
 		super.create();
 		add(new FlxText("Hello World", 32).screenCenter());
 	}
 
-	override public function update(elapsed:Float)
-	{
+	override public function update(elapsed:Float) {
 		super.update(elapsed);
 	}
 }
