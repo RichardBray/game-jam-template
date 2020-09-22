80e2a34d3ea7ae27d44a4be7b47851196b1b54b6
Richard Bray <richardbray89@gmail.com>
2020-09-22 17:26:06 +0100

    Prepare first shader example

---
+++ source/part1/PlayState.hx
@@ -0,0 +1,19 @@
+package part1;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.util.FlxColor;
+
+class PlayState extends FlxState {
+	override public function create() {
+		super.create();
+
+		FlxG.mouse.useSystemCursor = true;
+		bgColor = 0xffcccccc; // GREY
+
+		final box = new FlxSprite(50, 50);
+		box.makeGraphic(100, 100, FlxColor.BLUE);
+		add(box);
+	}
+}
